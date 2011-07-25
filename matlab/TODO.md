c685ee11caf294a6b83b3ccc6d97c66d3ea10384
Jesse Lu <mr.jesselu@gmail.com>
2011-07-24 23:10:23 -0700

    Decided to start new branch linear-ff.

--- matlab/TODO.md
+++ matlab/TODO.md
@@ -1,11 +1,14 @@
 Now...
 ======
 
-1.  "Balance" the top. update to reduce error.
-1.  Enhance the top. update test function to help do this.
+1.  Start branch with new, linear fractional-filling scheme.
+1.  Re-analyze error in update_dir by refining test.
+1.  If new scheme works better, re-write theory.
 
 Next...
 =======
 
+
+1.  Document update_dir test function.
 1.  Finish coding the island nucleation function
 1.  Document lso_priv_adjacents
--- matlab/lso_test_updatedir.m
+++ matlab/lso_test_updatedir.m
@@ -1,14 +1,15 @@
-function lso_test_updatedir(time_limit)
+function [err] = lso_test_updatedir(time_limit)
 % LSO_TEST_UPDATEDIR(TIME_LIMIT)
 % 
 % Description
 %     Run tests on LSO_UPDATEDIR for TIME_LIMIT seconds.
 
-fail_lim = 1e-6; % If error exceeds this, issue fail.  
+fail_lim = 0.01; % If error exceeds this, issue fail.  
 
 % Start tests.
 start_time = tic;
 
+err = [];
 while (toc(start_time) < time_limit)
     dims = randi(100, [1 2]); % Pick dimensions of grid.
 
@@ -16,23 +17,40 @@ while (toc(start_time) < time_limit)
     phi = lso_regularize(rand(dims) - rand(1));
     p = lso_fracfill(phi);
     dp = randn(dims);
-    dp = dp ./ norm(dp);
+    dp = dp ./ norm(dp(:));
+
+%     p1 = p + dp
+%     dp = 
 
     % Get the update direction and compute error.
     dphi = lso_updatedir(phi, dp);
-    err = abs(((p > -1) & (p < 1)) .* ...
-        (lso_fracfill(phi + 1e-5 * dphi) - (p + 1e-5 * dp)));
+    s = 1e-5;
+    e = abs(((p > -1) & (p < 1)) .* ...
+        (lso_fracfill(phi + s * dphi) - (p + s * dp)) ./ s);
+    err(end+1) = max(e(:)); 
 
     % Print results.
-    fprintf('(%d, %d):\t', dims);
-    if (max(err(:)) < fail_lim)
+    fprintf('(%d, %d):\t%e\t', dims, err(end));
+
+    if (err < fail_lim)
         fprintf('passed.\n');
     else
-        subplot 121; imagesc(err'); axis equal tight;
-        subplot 122; lso_plot(phi + 1e-5 * dphi);
+        [temp, ind] = max(e(:));
+        [i, j] = ind2sub(size(e), ind);
+        k = 2;
+        subplot 121; imagesc(e(i-k:i+k,j-k:j+k)'); axis equal tight;
+        f = phi + 1e-5 * dphi;
+        subplot 122; lso_plot(f(i-k:i+k, j-k:j+k));
+        phi(i,j)
+        dphi(i,j)
+        p(i,j)
+        dp(i,j)
 
         fprintf('FAILED! error (%e)\n', max(err(:)));
         return
     end
 end
+
+
+
 
